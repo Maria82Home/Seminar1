a768f8334643483ad75b3ef5c8a948805621c9d2
maria yudina <home82@list.ru>
2023-02-12 16:09:38 +0500

    First C# seminar

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+int num = -12;
+int num1 = 6;
+double bigNum = 12.45;
+bool isEven = true;
+string word = "flower";
+char symbol = 'A';
+
+Console.WriteLine("My numbers are " + num + " and " + num1);
+Console.WriteLine($"My numbers are {num} and {num1}");
+
+
+Console.Write("Input integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your number is " + num);
+*/
+
+//Seminar1
+//Task1. Напишите программу, которая на вход принимает
+//два числа и проверяет, является ли первое квадратом второго.
+/*
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+if(num1 == quad2)
+  Console.WriteLine("Yes");
+else
+  Console.WriteLine("No");
+*/
+
+//Task1. Напишите программу, которая на вход принимает
+//число N, а на выходе показывает все целые числа в промежутке от -N до N.
+
+/*
+Console.Write("Введите положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+  Console.Write(current + " ");
+  current++;
+}
+*/
+
